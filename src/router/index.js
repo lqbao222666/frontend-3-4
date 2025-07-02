a1c5267f996b516c31784954ab354ebf12ab4069
Le Quoc Bao <lqbao31082004@gmail.com>
2025-07-02 23:50:07 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,20 @@ import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 const routes = [
   {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+  },
+  {
     path: "/:pathMatch(.*)*",
     name: "notfound",
     component: () => import("@/views/NotFound.vue"),
+  },
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
   },
 ];
 const router = createRouter({
